2ec39ecd834381e42637f8f4d1243eabf63c216c
AWOS <uws0331@gmail.com>
2023-06-06 13:57:51 +0900

    refactor: chrome driver 설정하는 부분을 따로 뺌

--- src/login.py
+++ src/login.py
@@ -1,15 +1,15 @@
 import os
-import requests
-import selenium.webdriver as webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
-browser = webdriver.Chrome('D:/DanawaAutoCheck/src/chromedriver.exe')
+from set_chrome_driver import set_chrome_driver
+
+browser = set_chrome_driver()
 browser.get('https://auth.danawa.com/login?url=http%3A%2F%2Fwww.danawa.com%2Fmember%2FmyPage.php')
 browser.implicitly_wait(3)
 
 browser.find_element(By.ID, "danawa-member-login-snsButton-kakao").click()
-time.sleep(500)
+
+browser.switch_to.window(browser.window_handles[-1])
 
 print("login")
