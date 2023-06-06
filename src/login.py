b308a85d2fb0df88d2e443e5b7df93c96e612830
AWOS <uws0331@gmail.com>
2023-06-06 13:29:03 +0900

    feat: 다나와 홈페이지에서 로그인을 카카오로 하도록 설정

--- src/login.py
+++ src/login.py
@@ -1,4 +1,15 @@
 import os
 import requests
+import selenium.webdriver as webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+browser = webdriver.Chrome('D:/DanawaAutoCheck/src/chromedriver.exe')
+browser.get('https://auth.danawa.com/login?url=http%3A%2F%2Fwww.danawa.com%2Fmember%2FmyPage.php')
+browser.implicitly_wait(3)
+
+browser.find_element(By.ID, "danawa-member-login-snsButton-kakao").click()
+time.sleep(500)
 
 print("login")
